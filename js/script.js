d1dfbe38400657f7285945f383cd36d88dd09198
Mariana Dominguez <dominguezmariana483@gmail.com>
2024-08-23 10:50:51 -0300

    Alert arreglado para Recetas

--- js/script.js
+++ js/script.js
@@ -42,37 +42,6 @@ function mostrarSaludo() {
 // --------------------------   FIN DE FUNCION SALUDO   ----------------------
 
 
-// --------------------------   INICIO DE ALERT CON FRASE - recetas  ------------------
-
-// Verificamos si estamos en la página recetas.html dentro de la carpeta secciones
-if (window.location.pathname === "secciones/recetas.html") {
-    // Obtenemos el día de la semana (0 = Domingo, 6 = Sábado)
-    const diaSemana = new Date().getDay();
-
-    // Recetas promocionales según el día de la semana
-    const recetas = {
-        0: "Domingo: Refresca tu día con un Agua de pepino y limón.",
-        1: "Lunes: Disfruta de un Batido verde détox para comenzar la semana lleno de energía.",
-        2: "Martes: Prueba nuestra deliciosa Ensalada de quínoa y vegetales.",
-        3: "Miércoles: Saboréate con una Ensalada de garbanzos y atún, perfecta para un almuerzo ligero.",
-        4: "Jueves: Disfruta de un Pollo a la parrilla con espárragos para una cena saludable.",
-        5: "Viernes: ¡Sorpréndete con un Salmón al horno con verduras para terminar la semana laboral!",
-        6: "Sábado: Date un gusto con un Mousse de aguacate y chocolate o un Yogur griego con frutas y granola."
-    };
-
-    // Mostramos el alert con la receta del día
-    alert(recetas[diaSemana]);
-}
-
-
-
-// --------------------------    FIN DE ALERT CON FRASE  ----------------
-
-
-
-
-
-
 // ---------------------  INICIO BASE DE DATOS Y FUNCIONES - primeros auxilios ---------------
 // Inicialización de la base de datos de hospitales en formato JSON
 const hospitalesJSON = `
